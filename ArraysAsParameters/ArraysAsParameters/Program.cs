4a3a862037ce07fc823656c644e93e9c75beecde
Sidney Shafer <66838571+sidneyshafer@users.noreply.github.com>
2023-02-21 12:13:26 -0600

    Add files via upload

---
+++ ArraysAsParameters/ArraysAsParameters/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace ArraysAsParameters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] studentsGrades = new int[] { 15, 13, 8, 12, 6, 20, 14, 15 };
+            double averageResult = GetAverage(studentsGrades);
+            
+            int[] happiness = new int[] { 1, 2, 3, 4, 5 };
+            SunIsShining(happiness);
+
+            foreach(int grade in studentsGrades)
+            {
+                Console.WriteLine("Grade: {0}", grade);
+            }
+
+            Console.WriteLine("The grade point average is: {0}", averageResult);
+
+            Console.WriteLine("-------------------------------");
+
+            foreach(int num in happiness)
+            {
+                Console.WriteLine("Happy Num: {0}", num);
+            }
+            Console.ReadKey();
+
+        }
+
+        static double GetAverage(int[] gradesArray)
+        {
+            int size = gradesArray.Length;
+            double average;
+            int sum = 0;
+
+            for (int i = 0; i < size; i++)
+            {
+                sum += gradesArray[i];
+            }
+
+            // cast into double
+            average = Math.Round((double)sum / size, 0); // rounds to whole number
+            return average;
+        }
+
+        static void SunIsShining(int[] x)
+        {
+            for(int i = 0; i < x.Length; i++)
+            {
+                x[i] += 2;
+            }
+        }
+    }
+}
