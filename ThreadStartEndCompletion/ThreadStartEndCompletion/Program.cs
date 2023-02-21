386ed3020084e7b1fe7cf600d30b66cc8373f4a1
Sidney Shafer <66838571+sidneyshafer@users.noreply.github.com>
2023-02-21 06:49:06 -0600

    commit via upload

---
+++ ThreadStartEndCompletion/ThreadStartEndCompletion/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+
+namespace ThreadStartEndCompletion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var taskCompletionSource = new TaskCompletionSource<bool>();
+
+            //Thread variable
+            var thread = new Thread(() =>
+            {
+                //get id of specific thread
+                Console.WriteLine($"Thread number : {Thread.CurrentThread.ManagedThreadId} started");
+                Thread.Sleep(1000);
+                //set the taskComplectionSource to true
+                taskCompletionSource.TrySetResult(true);
+                Console.WriteLine($"Thread number : {Thread.CurrentThread.ManagedThreadId} ended");
+            });
+
+            //Console.WriteLine($"Thread number : {thread.ManagedThreadId}");
+            //sart thread
+            thread.Start();
+
+            var test = taskCompletionSource.Task.Result;
+
+            Console.WriteLine("Task was done : {0}", test);
+            
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ThreadsBasics/ThreadsBasics/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadsBasics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Console.WriteLine("Hello, World! 1");
+            //pauses program for 1000 milliseconds (1 second)
+            Thread.Sleep(1000); //generally don't do this in the Main thread
+
+            Console.WriteLine("Hello, World! 2");
+            Thread.Sleep(1000);
+
+            Console.WriteLine("Hello, World! 3");
+            Thread.Sleep(1000);
+
+            Console.WriteLine("Hello, World! 4");
+            */
+
+            //Threads : a piece of work that can be executed. They can also be executed at the same time
+
+            // Create and start a new thread
+            new Thread(() =>
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("Thread 1");
+            }).Start();
+
+            new Thread(() =>
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("Thread 2");
+            }).Start();
+
+            new Thread(() =>
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("Thread 3");
+            }).Start();
+
+            new Thread(() =>
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("Thread 4");
+            }).Start();
+
+            Console.ReadKey();
+        }
+    }
+}
