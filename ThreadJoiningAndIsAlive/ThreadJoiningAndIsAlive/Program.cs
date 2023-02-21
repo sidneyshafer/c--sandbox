8c314ea86832400561efbac897b4017e361ad1b8
Sidney Shafer <66838571+sidneyshafer@users.noreply.github.com>
2023-02-21 06:48:13 -0600

    commit via upload

---
+++ ThreadJoiningAndIsAlive/ThreadJoiningAndIsAlive/Program.cs
@@ -0,0 +1,62 @@
+﻿using System.Threading;
+
+namespace ThreadJoiningAndIsAlive
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Main Thread started");
+
+            Thread thread1 = new Thread(Thread1Function);
+            Thread thread2 = new Thread(Thread2Function);
+            thread1.Start();
+            thread2.Start();
+
+            //Join() blocks the called thread. In this case, it is blocking the main thread
+            //thread1.Join();
+            //Console.WriteLine("Thread1Function done");
+
+            if (thread1.Join(1000))
+            {
+                Console.WriteLine("Thread1Function done within 1 sec");
+            }
+            else
+            {
+                Console.WriteLine("Thread1Function not done within 1 sec");
+            }
+
+            thread2.Join();
+            Console.WriteLine("Thread2Function done");
+
+            for (int i = 0; i < 10; i++)
+            {
+                //if thread is still running
+                if (thread1.IsAlive)
+                {
+                    Console.WriteLine("Thread1 is still running");
+                    Thread.Sleep(300);
+                }
+                else
+                {
+                    Console.WriteLine("Thread1 completed");
+                }
+            }
+
+            
+            Console.WriteLine("Main Thread ended");
+        }
+
+        public static void Thread1Function()
+        {
+            Console.WriteLine("Thread1Function started");
+            Thread.Sleep(3000);
+            Console.WriteLine("Thread1Function coming back to Main Thread");
+        }
+
+        public static void Thread2Function()
+        {
+            Console.WriteLine("Thread2Function started");
+        }
+    }
+}
---
+++ ThreadPoolsAndBackground/ThreadPoolsAndBackground/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Linq;
+
+namespace ThreadPoolsAndBackground
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Threads are used to run specific work/work items
+
+            //set thread to background thread
+            new Thread(() =>
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("Thread 1");
+            })
+            { IsBackground = true }.Start();
+
+            //Create multiple threads
+            Enumerable.Range(0,100).ToList().ForEach(f =>
+            {
+                ThreadPool.QueueUserWorkItem((o) =>
+                {
+                    Console.WriteLine($"Thread number: {Thread.CurrentThread.ManagedThreadId} started");
+                    Thread.Sleep(1000);
+                    Console.WriteLine($"Thread number: {Thread.CurrentThread.ManagedThreadId} ended");
+                });
+
+                /*
+                new Thread(() =>
+                {
+                    Console.WriteLine($"Thread number: {Thread.CurrentThread.ManagedThreadId} started");
+                    Thread.Sleep(1000);
+                    Console.WriteLine($"Thread number: {Thread.CurrentThread.ManagedThreadId} ended");
+                }).Start();
+                */
+            });
+
+            Console.ReadKey();
+        }
+    }
+}
