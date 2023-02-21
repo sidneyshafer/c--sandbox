9e7d4c9cada239ec4418602b19236d5bbe66b87f
Sidney Shafer <66838571+sidneyshafer@users.noreply.github.com>
2023-02-21 12:04:13 -0600

    Add files via upload

---
+++ WPF_08/WPF_08/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_08
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        //Create object of Sum class
+        public Sum Sumobj { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Sumobj = new Sum { Num1 = "1", Num2 = "3" };
+            this.DataContext = Sumobj;
+        }
+    }
+}
