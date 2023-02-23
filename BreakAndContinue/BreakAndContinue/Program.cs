fb2fbdea1c7a2745ea0806b0a08a3605d1761d4a
Sidney Shafer <sidney@mnshafers.com>
2023-02-23 06:42:29 -0600

    commit files via upload

---
+++ BreakAndContinue/BreakAndContinue/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace BreakAndContinue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 0; i < 10; i++)
+            {
+
+                if (i == 3)
+                {
+                    Console.WriteLine("At 3 we continue!");
+                    // skips to next number
+                    continue;
+                }
+                else if (i == 6)
+                {
+                    Console.WriteLine("At 6 we break!");
+                    // exits for loop
+                    break;
+                }
+
+                Console.WriteLine(i);
+            }
+
+            Console.Read();
+        }
+    }
+}
