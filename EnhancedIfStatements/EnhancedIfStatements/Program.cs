473bd4e2ac0001ab5e1d80a7973c2d85d665306a
Sidney Shafer <sidney@mnshafers.com>
2023-03-06 07:30:26 -0600

    Add files via upload

---
+++ EnhancedIfStatements/EnhancedIfStatements/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace EnhancedIfStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Entire if statement in one line (short cut if statements)
+
+            /* General Syntax:
+                    condition ? first_expression : second_expression
+
+               * Condition has to be either true or false
+               * The conditional operator is right - associative
+               * The expression: 
+                    a ? b: c ? d : e 
+                    
+                    is evaluated as:
+                    a ? b : (c ? d : e), NOT --> (a ? b : c) ? d: e
+               
+               * The conditional operator cannot be overloaded.
+            */
+
+
+            int temperature = -5;
+            string stateOfMatter;
+
+            
+            if (temperature < 0)
+            {
+                stateOfMatter = "solid";
+            }
+            else
+            {
+                stateOfMatter = "liquid";
+            }
+
+            Console.WriteLine("State of matter is {0}", stateOfMatter);
+            temperature += 30;
+
+            // Above if statement as Ternary Operator:
+            stateOfMatter = temperature < 0 ? "solid" : "liquid";
+
+            Console.WriteLine("State of matter is {0}", stateOfMatter);
+
+            // Challenge: add gas state of matter options
+            temperature += 100;
+            stateOfMatter = temperature > 100 ? "gas" : temperature < 0 ? "solid" : "liquid";
+            
+            Console.WriteLine("State of matter is {0}", stateOfMatter);
+
+            Console.ReadKey();
+
+        }
+    }
+}
