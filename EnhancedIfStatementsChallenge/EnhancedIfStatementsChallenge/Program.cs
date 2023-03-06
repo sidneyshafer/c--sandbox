9bb42e0b371dfef5a6ba85a8bd3f976d857655ad
Sidney Shafer <sidney@mnshafers.com>
2023-03-06 07:30:55 -0600

    Add files via upload

---
+++ EnhancedIfStatementsChallenge/EnhancedIfStatementsChallenge/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace EnhancedIfStatementsChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputTemperature = 0;
+            string tempMessage = string.Empty;
+            string inputValue = string.Empty;
+
+            // Take input from the console
+            Console.WriteLine("What is the temperature today?");
+            inputValue = Console.ReadLine();
+
+            // Validate the input as valid input integer value
+            bool validInteger = int.TryParse(inputValue, out inputTemperature);
+
+            // variable = condition ? ifTrue : ifFalse
+            tempMessage = inputTemperature <= 15 ? "It is too cold here" :
+                inputTemperature >= 16 && inputTemperature <= 28 ? "It is ok" : "It is hot here";
+
+            Console.WriteLine(tempMessage);
+            Console.ReadKey();
+        }
+    }
+}
