b5662f5ac299ea818ba71eb242c665e8000eda9c
Sidney Shafer <sidney@mnshafers.com>
2023-04-24 12:49:41 -0500

    Add files via upload

---
+++ IEnumerableAndIEnumerator/IEnumerableAndIEnumerator/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace IEnumerableAndIEnumerator
+{
+    class Program
+    {
+        // 1. IEnumerable <T> for generic collections
+        // 2. IEnumerable for non generic collections
+
+        // - used to iterate through a collection
+        // - can use foreach loops
+        // - IEnumarble makes objects countable
+
+        /// <summary> 
+        /// IEnumerable contains a single method that you must implement when
+        /// implementing this interface
+        /// GetEnumerator(), which returns an IEnumerator<T> object.
+        /// The returned IEnumerator<T> provides the ability to iterate through
+        /// the collection by exposing a Current property that points at the object 
+        /// we are currently at in the collection
+        /// </summary>
+
+        /// when it is recommended to use the IEnumerable interface:
+        /// - when yuor collection represents a massive database table,
+        /// you don't want to copy the entire thing into memory and cause performance issues
+        /// in your application.
+        /// When it is NOT recommended to use the IEnumerable interface:
+        /// - when you need the results right away are are possible mutating / editing
+        /// the objects later on. In this case, it is better to use an Array or List
+        static void Main(string[] args)
+        {
+            DogShelter shelter = new DogShelter();
+
+            foreach (Dog dog in shelter)
+            {
+                if(!dog.IsNaughtyDog)
+                {
+                    dog.GiveTreat(3);
+                }
+                else
+                {
+                    dog.GiveTreat(1);
+                }
+            }
+        }
+
+    }
+
+    class Dog
+    {
+        // name of the dog
+        public string Name { get; set; }
+        // is this a naughty dog?
+        public bool IsNaughtyDog { get; set; }
+
+        // simple constructor
+        public Dog(string name, bool isNaughtyDog)
+        {
+            this.Name = name;
+            this.IsNaughtyDog = isNaughtyDog;
+        }
+
+        // this method will print how many treats the dog receives
+        public void GiveTreat(int numberOfTreats)
+        {
+            //print a message containing the number of treats and the name of the dog
+            Console.WriteLine($"Dog: {Name} got {numberOfTreats}! " +
+                $"{Name} said whoof {numberOfTreats} times!");
+        }
+    }
+
+    //a class named DogShelter that contains a generic collection of type Dog
+    //must implement IEnumerable<T> to iterate through DogShelter list
+    class DogShelter : IEnumerable<Dog>
+    {
+        //list of type List<Dog>
+        public List<Dog> dogs;
+        //this constructor will initialize the dogs list with some values
+        public DogShelter()
+        {
+            //initialize the dogs list using the collection-initializer
+            dogs = new List<Dog>()
+            {
+                new Dog("Casper", false),
+                new Dog("Sif", true),
+                new Dog("Oreo", false),
+                new Dog("Pixel", false),
+            };
+        }
+
+        IEnumerator<Dog> IEnumerable<Dog>.GetEnumerator()
+        {
+            return dogs.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ IEnumerableDemo/IEnumerableDemo/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IEnumerableDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //creating a generic IEnumerable variable that takes any collection of type int
+            //we will use this variable to store the collections that will get returned by
+            //the GetCollection() method
+            IEnumerable<int> unknownCollection;
+            unknownCollection = GetCollection(1);
+
+            Console.Write("This was a List<int> : ");
+            //for each number in the collection we got back from GetCollection(1);
+            foreach (int num in unknownCollection)
+            {
+                Console.Write(num + " ");
+            }
+            //new line
+            Console.WriteLine();
+
+
+            //call GetCollection() with option 2 which will return a Queue<int>
+            //but we will store it in the base type of generic collections
+            unknownCollection = GetCollection(2);
+
+            Console.Write("This was a Queue<int> : ");
+            //for each number in the collection we got back from GetCollection(2);
+            foreach (int num in unknownCollection)
+            {
+                Console.Write(num + " ");
+            }
+            //new line
+            Console.WriteLine();
+
+
+            //call GetCollection() with option = 5 which will return an arrat int[] but we
+            // will store it in the base type of generic collections
+            unknownCollection = GetCollection(5);
+
+            Console.Write("This was an array of type int[] : ");
+            //for each number in the collection we got back from GetCollection(2);
+            foreach (int num in unknownCollection)
+            {
+                Console.Write(num + " ");
+            }
+            //new line
+            Console.WriteLine();
+
+
+            Console.ReadKey();
+
+        }
+
+        //method return an IEnumeranle<int> of type int
+        //reutrns a type of list depending on what the option is
+        static IEnumerable<int> GetCollection(int option)
+        {
+            //create a list of numbers and initialize it
+            List<int> numbersList = new List<int>() { 1, 2, 3, 4, 5 }; 
+
+            //create a Queue of values
+            Queue<int> numbersQueue = new Queue<int>();
+            //add values to the queue
+            numbersQueue.Enqueue(6);
+            numbersQueue.Enqueue(7);
+            numbersQueue.Enqueue(8);
+            numbersQueue.Enqueue(9);
+            numbersQueue.Enqueue(10);
+
+            //if option is 1
+            if (option == 1)
+            {
+                //return the list of type List<int>
+                return numbersList;
+            } //if the option is 2
+            else if (option == 2)
+            {
+                //return the queue of tpye<int>
+                return numbersQueue;
+            }// otherwise
+            else
+            {
+                //return an array of numbers initialized with some numbers
+                return new int[] { 11, 12, 13, 14, 15 };
+            }
+
+        }
+    }
+}
