3c400412de378d134221f2332b4de74a72b73e5b
Sidney Shafer <sidney@mnshafers.com>
2023-04-24 10:49:02 -0500

    Add files via upload

---
+++ Hashtables/Hashtables/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections;
+
+namespace Hashtables
+{
+    class Program
+    {
+        // Key - Value
+        static void Main(string[] args)
+        {
+            // Uses System.Collections;
+            // Initialze an empty hashtable
+            Hashtable studentsTable = new Hashtable();
+
+            // Create student objects
+            Student stud1 = new Student(1, "Maria", 3.8f);
+            Student stud2 = new Student(2, "Jason", 3.0f);
+            Student stud3 = new Student(3, "Clara", 4.0f);
+            Student stud4 = new Student(4, "Steve", 3.5f);
+
+            // Add data
+            // stud1.Id - Key
+            // stud1 - Value
+            studentsTable.Add(stud1.Id, stud1);
+            studentsTable.Add(stud2.Id, stud2);
+            studentsTable.Add(stud3.Id, stud3);
+            studentsTable.Add(stud4.Id, stud4);
+
+            // Retrieve entries - individual item with known ID
+            Student storedStudent1 = (Student)studentsTable[stud1.Id];
+
+            Console.WriteLine("Student ID: {0}\nName: {1}\nGPA: {2}", storedStudent1.Id, storedStudent1.Name, storedStudent1.GPA);
+            Console.WriteLine();
+
+
+            // Retrieve all values from hashtable
+            foreach (DictionaryEntry entry in studentsTable)
+            {
+                Student temp = (Student)entry.Value;
+                Console.WriteLine("Student ID: {0}", temp.Id);
+                Console.WriteLine("Student Name: {0}", temp.Name);
+                Console.WriteLine("Student GPA: {0}", temp.GPA);
+                Console.WriteLine();
+            }
+
+            // Easier way than the code above
+            foreach (Student value in studentsTable.Values)
+            {
+                Console.WriteLine("Student ID: {0}", value.Id);
+                Console.WriteLine("Student Name: {0}", value.Name);
+                Console.WriteLine("Student GPA: {0}", value.GPA);
+                Console.WriteLine();
+            }
+
+            
+            Console.ReadKey();
+        }
+    }
+
+    // Create new class
+    class Student
+    {
+        // Id property
+        public int Id { get; set; }
+
+        // Name property
+        public string Name { get; set; }
+
+        // GPA property
+        public float GPA { get; set; }
+
+        // Constructor
+        public Student(int id, string name, float GPA)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.GPA = GPA;
+        }
+    }
+}
---
+++ HashtablesChallenge/HashtablesChallenge/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+
+namespace HashtablesChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Array of students
+            Student[] students = new Student[5];
+            students[0] = new Student(1, "Denis", 88);
+            students[1] = new Student(2, "Olaf", 97);
+            students[2] = new Student(6, "Ragner", 65);
+            students[3] = new Student(1, "Luise", 73);
+            students[4] = new Student(5, "Levi", 58);
+
+            // Create a new hashtable
+            Hashtable table = new Hashtable();
+
+            // Iterate through students array
+            foreach (Student s in students)
+            {
+                if(!table.ContainsKey(s.Id))
+                {
+                    table.Add(s.Id, s);
+                    Console.WriteLine("Student with ID {0} was added.", s.Id);
+                }
+                else
+                {
+                    Console.WriteLine("Sorry, A student with the same ID already exists. ID: {0}", s.Id);
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+    class Student
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public float GPA { get; set; }
+
+        public Student(int id, string name, float GPA)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.GPA = GPA;
+        }
+    }
+}
