f5174e7006389c9e65b1ccef2472a56c7e84dbf6
Sidney Shafer <sidney@mnshafers.com>
2023-04-24 13:09:19 -0500

    Add files via upload

---
+++ WhileLoops/WhileLoops/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace WhileLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // While Loops
+            int i = 0;
+            while (i < 10)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+            // output = 0,1,2,3,4,5,6,7,8,9
+            int counter = 0;
+            string enteredText = "";
+            while (enteredText.Equals(""))
+            {
+                Console.WriteLine("Please press enter to increase by one." +
+                    "Anything else, enter if you want to finish counting.");
+                enteredText = Console.ReadLine();
+                
+                counter++;
+                Console.WriteLine("Current people count it {0}", counter);
+            }
+            Console.WriteLine("{0} people are on the bus.", counter);
+            Console.Read();
+        }
+    }
+}
---
+++ WriteToFile/WriteToFile/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+
+namespace WriteToFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Method 1
+            string[] lines = { "First : 250", "Second : 242", "Third : 240" };
+            File.WriteAllLines(@"C:\Users\shafe\OneDrive\Documents\Web Development Courses and Resources\Programming for Beginners in C#\Assets\highscores.txt", lines);
+
+            // Method 3
+            /*
+            Console.WriteLine("Enter file name : ");
+            string fileName = Console.ReadLine();
+
+            Console.WriteLine("Enter file contents : ");
+            string input = Console.ReadLine();
+            */
+
+            // File.WriteAllText(@"C:\Users\shafe\OneDrive\Documents\Web Development Courses and Resources\Programming for Beginners in C#\Assets\" + fileName + ".txt", input);
+
+            // Method 3
+            using(StreamWriter file = new StreamWriter(@"C:\Users\shafe\OneDrive\Documents\Web Development Courses and Resources\Programming for Beginners in C#\Assets\myText.txt"))
+            {
+                foreach(string line in lines)
+                {
+                    if (line.Contains("Third"))
+                    {
+                        file.WriteLine(line);
+                    }
+                }
+            }
+
+            using(StreamWriter file = new StreamWriter(@"C:\Users\shafe\OneDrive\Documents\Web Development Courses and Resources\Programming for Beginners in C#\Assets\myText.txt", true))
+            {
+                file.WriteLine("Additional line!!!");
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
