0ceb4f4ee99adab80f75d34e161109b82618da4c
Sidney Shafer <sidney@mnshafers.com>
2023-04-24 10:47:13 -0500

    Add files via upload

---
+++ ForEachLoop/ForEachLoop/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ForEachLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Foreach Loop
+            // create a new array
+            int[] nums = new int[10];
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                nums[i] = i + 10;
+            }
+
+            for(int j = 0; j < nums.Length; j++)
+            {
+                Console.WriteLine("Element {0} = {1}", j, nums[j]);
+            }
+            Console.WriteLine("----------------");
+
+            // counter
+            int counter = 0;
+            foreach(int k in nums)
+            {
+                Console.WriteLine("Element {0} = {1}", counter, k);
+                counter++;
+            }
+            Console.WriteLine("----------------");
+
+            // Create a friend list array
+            string[] friends = { "Allie", "Lisa", "Brent", "Tryg" };
+            foreach(string friend in friends)
+            {
+                Console.WriteLine("Hello {0}, my friend!", friend);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ForeachAndSwitchChallenge/ForeachAndSwitchChallenge/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace ForeachAndSwitchChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Boolean valid = false;
+            string inputValueType = string.Empty;
+
+            Console.WriteLine("Enter a value : ");
+            string inputValue = Console.ReadLine();
+
+            Console.WriteLine("Select the data type to validate the input you entered.");
+            Console.WriteLine("Press 1 for String");
+            Console.WriteLine("Press 2 for Integer");
+            Console.WriteLine("Press 3 for Boolean");
+
+            Console.Write("Enter : ");
+            int inputType = Convert.ToInt32(Console.ReadLine());
+
+            switch (inputType)
+            {
+                case 1:
+                    // check for string
+                    valid = IsAllAlphabetic(inputValue);
+                    inputValueType = "String";
+                    break;
+
+                case 2:
+                    // check for int
+                    int retValue = 0;
+                    valid = int.TryParse(inputValue, out retValue);
+                    inputValueType = "Integer";
+                    break;
+
+                case 3:
+                    bool retFlag = false;
+                    // check for boolean
+                    valid = Boolean.TryParse(inputValue, out retFlag);
+                    inputValueType = "Boolean";
+                    break;
+
+                default:
+                    inputValueType = "unknown";
+                    Console.WriteLine("Something went wrong, not able to detect the input type.");
+                    break;
+            }
+
+            Console.WriteLine("You have entered a value : {0}", inputValue);
+
+            if (valid)
+            {
+                Console.WriteLine("It is valid : {0}", inputValueType);
+            }
+            else
+            {
+                Console.WriteLine("It is an invalid : {0}", inputValueType);
+            }
+
+            Console.ReadKey();
+
+            /// <summary>
+            ///  Function to check if the input string is a pure letter string.
+            /// </summary>
+            /// <param name="value"></param>
+            /// <returns></returns>
+            static bool IsAllAlphabetic(string value)
+            {
+                foreach (char c in value)
+                {
+                    if (!char.IsLetter(c))
+                        return false;
+                }
+
+                return true;
+            }
+        }
+    }
+}
