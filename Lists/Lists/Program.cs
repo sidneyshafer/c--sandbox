c8e5b6a46c710bda8f12783cf200ca1215a7460b
Sidney Shafer <sidney@mnshafers.com>
2023-04-24 12:55:57 -0500

    Add files via upload

---
+++ Lists/Lists/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Lists
+             * Compared to arrays, the group of objects/values you work with can grow
+               and shrink dynamically.
+             * A collection is a class and it requires an instance of the class before adding
+               elements to that collection.
+             * For collections that only contain one data type use the System.Collection.Generic namespace
+            */
+
+            // List declaration examples
+            var numbers = new List<int>(); // List of type int without values
+            var numbers2 = new List<int> { 1, 5, 35, 100 }; // List of type int with values
+
+            // Adding and Removing Values
+            numbers.Add(7); // Adds an integer with a value of 7 to the list
+            numbers.Add(9);
+            numbers.Remove(7); // Removes an element with a value of 7
+
+            int index = 0;
+            numbers.RemoveAt(index); // Removes an elements at a specific position (index)
+
+            // Getting a value
+            int value = numbers2[1]; // value will be 5
+
+            // Clearing a list
+            numbers.Clear(); // Deletes every elements in the list
+
+            // Lists and Loops
+            var numbers3 = new List<int> { 5, 10, 15, 20, 25, 30, 35, 40 };
+
+            foreach (int element in numbers3) // foreach loop
+            {
+                Console.Write(element + " ");
+            }
+
+            Console.WriteLine();
+
+            for (int i = 0; i < numbers3.Count; i++) // for loop
+            {
+                Console.Write(numbers3[i] + " ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ LoopsChallenge/LoopsChallenge/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace LoopsChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Set variables
+            string input = "0";
+            int count = 0;
+            int total = 0;
+            int currentNumber = 0;
+
+            // When -1 is entered, the program ends and no more values are added
+
+            while(input != "-1")
+            {
+                Console.WriteLine("Last number entered was {0}", currentNumber);
+                Console.WriteLine("Current amount of entries is {0}", count);
+                Console.WriteLine("Please enter -1 to calculate the average");
+                Console.WriteLine("Please enter the next score:");
+
+                input = Console.ReadLine();
+                if (input.Equals("-1"))
+                {
+                    Console.WriteLine("------------------------------------------");
+                    // Calculate average and let user know
+                    double average = Math.Round((double)total / (double)count, 2);
+                    Console.WriteLine("Total entries is: {0}", count);
+                    Console.WriteLine("Average score is: {0}", average);
+                }
+                // Check that user input is a number and that it is between 0 and 20
+                if(int.TryParse(input, out currentNumber) && currentNumber > 0 && currentNumber < 21)
+                {
+                    total += currentNumber;
+                }
+                else
+                {   // if input does not equal -1
+                    if(!(input.Equals("-1")))
+                    {
+                        Console.WriteLine("Please enter a value between 1 and 20.");
+                        Console.WriteLine("------------------------------------------");
+                    }
+                    continue;
+                }
+
+                count++;
+                
+            }
+
+            Console.Read();
+        }
+    }
+}
