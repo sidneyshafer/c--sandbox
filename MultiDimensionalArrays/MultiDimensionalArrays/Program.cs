2bb26f61b97a3db354fe84d0cc5a565bebb4d5c0
Sidney Shafer <sidney@mnshafers.com>
2023-04-24 12:59:36 -0500

    Add files via upload

---
+++ MultiDimensionalArrays/MultiDimensionalArrays/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace MultiDimensionalArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declare 2D Array
+            string[,] matrix;
+
+            // Declare 3D Array
+            int[,,] threeD;
+
+            // 4D
+            // string[,,,] fourD;
+
+            // 5D
+            // int[,,,,] fiveD;
+
+            // Initialize Arrays
+            // two dimensional array
+            int[,] array2D = new int[,]
+            {
+                {1, 2, 3}, // row 0
+                {4, 5, 6}, // row 1
+                {7, 8, 9}, // row 2
+            };
+
+            // Access number 5
+            Console.WriteLine("Central value is {0}.", array2D[1, 1]);
+            // Get number 7
+            Console.WriteLine("The first number in the third row is {0}.", array2D[2, 0]);
+
+            // 3D Array
+            string[,,] array3D = new string[,,]
+            {
+                {
+                    {"000", "001"},
+                    {"010", "011"},
+                    {"Hi", "Hello"}
+                },
+                {
+                    {"100", "101"},
+                    {"110", "111"},
+                    {"By", "See Ya" }
+                }
+            };
+
+            Console.WriteLine("First value in 3D array is {0}.", array3D[0, 0, 0]);
+            Console.WriteLine("The value is '{0}'.", array3D[0,2,0]);
+            Console.WriteLine("---------------------------");
+
+            string[,] array2DString = new string[3, 2] 
+            { 
+                { "one", "two", }, 
+                { "three", "four" }, 
+                { "five", "six" } 
+            };
+
+            Console.WriteLine("Value is {0}", array2DString[1,1]);
+
+            // Change values in array
+            array2DString[1, 1] = "chicken";
+
+            Console.WriteLine("The new value is {0}", array2DString[1, 1]);
+
+            // Find dimensions of array
+            int dimensions = array2DString.Rank;
+            Console.WriteLine("The dimensions are {0}", dimensions);
+
+            int[,] array2D2 = { { 1, 2 }, { 3, 4 } };
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ NestedForLoopsAnd2DArrays/NestedForLoopsAnd2DArrays/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace NestedForLoopsAnd2DArrays
+{
+    class Program
+    {
+        static int[,] matrix =
+        {
+            {1,2,3},
+            {4,5,6},
+            {7,8,9},
+        };
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("2D array printed using foreach loop:");
+            foreach (int item in matrix)
+            {
+                Console.Write(item + " "); // output = 1 2 3 4 5 6 7 8 9
+            }
+
+            // Above foreach loop as a nested for loop
+            Console.WriteLine("\n2D array printed using nested for loop:");
+            // outer for loop
+            // iterates through rows in array
+            // GetLength(0) --> first dimension
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                // inner for loop
+                // iterates through columns in array
+                // GetLength(1) --> second dimension
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    // Console.Write(matrix[i, j] + " "); // output = 1 2 3 4 5 6 7 8 9
+                    if (matrix[i, j] % 2 == 0) // (matrix[i, j] % 2 == 1) --> prints odd numbers
+                    {
+                        Console.Write(matrix[i, j] + " "); // output = 2 4 6 8
+
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+            }
+
+            // Print diagonal values in matrix
+            Console.WriteLine("\nDiagonal Values in 2D matrix.");
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+
+                    if (i == j)
+                    {
+                        Console.Write(matrix[i, j] + " "); // output = 1 5 9
+
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+            }
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                // this loop goes through each element in the diagonal
+                Console.WriteLine(matrix[i,i]);
+            }
+
+            // Show diagonal going down in other direction
+            for (int i = 0, j = 2; i < matrix.GetLength(0); i++, j--)
+            {
+                Console.WriteLine(matrix[i,j]); // output = 3 5 7
+            }
+
+            Console.WriteLine("");
+            Console.ReadKey();
+        }
+    }
+}
