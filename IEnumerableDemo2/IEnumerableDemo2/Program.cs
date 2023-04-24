ce60930cd8156da219e786ff0ec72ae3e033818e
Sidney Shafer <sidney@mnshafers.com>
2023-04-24 12:50:32 -0500

    Add files via upload

---
+++ IEnumerableDemo2/IEnumerableDemo2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IEnumerableDemo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //a list of type List<int> initialized with some numbers
+            List<int> numberList = new List<int>() { 2, 8, 6 };
+            //an array of type int[] initialized with some numbers
+            int[] numberArray = new int[] { 1, 7, 1, 3 };
+            
+            //new line
+            Console.WriteLine(" ");
+            //call CollectionSum() and pass the list to it
+            CollectionSum(numberList);
+
+            //new line
+            Console.WriteLine(" ");
+            //call CollectionSum() and pass the array to it
+            CollectionSum(numberArray);
+        }
+
+        static void CollectionSum(IEnumerable<int> anyCollection)
+        {
+            //sum variable to store the sum of the numbers in anyCollection
+            int sum = 0;
+
+            //for each number in the collection passed to this method
+            foreach(int num in anyCollection)
+            {
+                //add the num value to sum
+                sum += num;
+            }
+
+            //print the sum
+            Console.WriteLine("Sum is : {0}", sum);
+        }
+    }
+}
---
+++ IfStatementsChallengeOne/IfStatementsChallengeOne/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace IfStatementsChallengeOne
+{
+    class Program
+    {
+        // Global Variables
+        // Created globaly so they can be used in multiplt methods
+        static string username;
+        static string password;
+        static void Main(string[] args)
+        {
+            Register();
+            Login();
+            Console.Read();
+        }
+
+        // Create method to register a user
+        public static void Register()
+        {
+            Console.WriteLine("Please enter your username:");
+            username = Console.ReadLine();
+            Console.WriteLine("Please enter your password:");
+            password = Console.ReadLine();
+            Console.WriteLine("Registration completed");
+            Console.WriteLine("--------------------------------");
+        }
+
+        public static void Login()
+        {
+            Console.WriteLine("Please enter your username:");
+            if (username == Console.ReadLine())
+            {
+                Console.WriteLine("Please enter your password:");
+                if (password == Console.ReadLine())
+                {
+                    Console.WriteLine("Login successful");
+                }
+                else
+                {
+                    Console.WriteLine("Login failed, wrong password. Restart Program");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Loging failed, wrong username. Restart Program");
+            }
+        }
+    }
+}
