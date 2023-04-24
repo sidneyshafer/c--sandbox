8dc23980c2206fec88125a02fcf3a06d17d4c187
Sidney Shafer <sidney@mnshafers.com>
2023-04-24 12:58:01 -0500

    Add files via upload

---
+++ MainArgs/MainArgs/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace MainArgs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // string[] args --> provides necessary input in order for application to run
+
+            if (args.Length == 0)
+            {
+                Console.WriteLine("This is a smart app that uses args ;). Please provide arguments next time. Pass 'help' if you want to get more details.");
+                Console.ReadKey();
+                return;
+            }
+
+            // check if the first command we get is help
+            if (args[0] == "help")
+            {
+                //display manual
+                Console.WriteLine("********************* Instructions:");
+                Console.WriteLine("* use one of the following commands followed by 2 numbers");
+                Console.WriteLine("* 'add' : to add 2 numbers");
+                Console.WriteLine("* 'sub' : to subtract 2 numbers");
+                Console.WriteLine("* use this format : sub (or) add num1 num2");
+                Console.WriteLine("*********************");
+
+                //pause
+                Console.ReadKey();
+                return;
+            }
+
+            //need 3 arguments
+            //check length of args
+            if (args.Length != 3)
+            {
+                Console.WriteLine("Invalid arguments, please use the 'help' command for instructions");
+                //pause
+                Console.ReadKey();
+                //quit the app
+                return;
+            }
+
+            // try parsting input
+            bool isNum1Parsed = float.TryParse(args[1], out float num1);
+            bool isNum2Parsed = float.TryParse(args[2], out float num2);
+
+            if (!isNum1Parsed || !isNum2Parsed)
+            {
+                Console.WriteLine("Invalid arguments, please use the 'help' command for instructions.");
+                //pause
+                Console.ReadKey();
+                //quit the app
+                return;
+            }
+
+            // a variable to store the results
+            float result;
+
+            switch (args[0])
+            {
+                //case 1 : 'add' add tje two numbers and print the value
+                case "add":
+                    result = num1 + num2;
+                    Console.WriteLine($"The sum of {num1} + {num2} = {result}");
+                    break;
+
+                //case 1 : 'add' add tje two numbers and print the value
+                case "sub":
+                    result = num1 - num2;
+                    Console.WriteLine($"The sum of {num1} - {num2} = {result}");
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid arguments, please use the 'help' command for instructions.");
+                    break;
+            }
+
+            //Console.WriteLine("Hello " + args[0]);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MathClass/MathClass/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace MathClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Round up a value --> Ceiling() Method
+            Console.WriteLine("Ceiling : " + Math.Ceiling(15.3)); // 16
+            
+            // Rounds down a value --> Floor() Method
+            Console.WriteLine("Floor : " + Math.Floor(16.9)); // 16
+            Console.WriteLine("\n");
+
+
+            // Min() and Max() Method
+            int num1 = 13;
+            int num2 = 9;
+            Console.WriteLine("Lower of num1 ({0}) and num2 ({1}) is : {2}",
+                num1, num2, Math.Min(num1, num2)); // 9
+
+            Console.WriteLine("Higher of num1 ({0}) and num2 ({1}) is : {2}",
+                num1, num2, Math.Max(num1, num2)); // 13
+            Console.WriteLine("\n");
+
+
+            // Pow() Method
+            Console.WriteLine("3 to the power of 5 is : " + Math.Pow(3, 5));
+
+            // PI Constant / Attribute
+            Console.WriteLine("PI is : " + Math.PI);
+
+            // Square Root --> Sqrt() Method
+            Console.WriteLine("The square root of 25 is : " + Math.Sqrt(25));
+            Console.WriteLine("\n");
+
+            // Absolute value --> Abs() Method
+            Console.WriteLine("Absolute value of -25 is : " + Math.Abs(-25));
+
+            // Cos() Method
+            Console.WriteLine("cos of 1 is : " + Math.Cos(1));
+
+            Console.ReadKey();
+        }
+    }
+}
